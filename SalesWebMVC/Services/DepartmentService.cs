78c7405fb01137b32751c7300fcc471b0ca10174
Deividy Santos <deividyosantos27@gmail.com>
2021-03-23 15:45:24 -0400

    Acrescentando SellerViewModel e DepartmentService

---
+++ SalesWebMVC/Services/DepartmentService.cs
@@ -0,0 +1,28 @@
+﻿using SalesWebMVC.Data;
+using SalesWebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Services
+{
+    public class DepartmentService
+    {
+        private readonly SalesWebMVCContext _salesWebMVC;
+
+        public DepartmentService(SalesWebMVCContext salesWebMVC)
+        {
+            _salesWebMVC = salesWebMVC;
+        }
+    
+        public List<Department> FindAll()
+        {
+            return _salesWebMVC.Department.OrderBy(x => x.Name).ToList();
+        }
+    
+    
+    }
+
+
+}
