8c7cea099f3f60c38b882b08ae09473d52583843
Deividy Santos <deividyosantos27@gmail.com>
2021-03-23 15:26:37 -0400

    Acrescentando DepartmentId a Seller

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -11,8 +11,8 @@ namespace SalesWebMVC.Models
         public string Email { get; set; }
         public DateTime BithDay { get; set; }
         public double BaseSalary { get; set; }
-
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
